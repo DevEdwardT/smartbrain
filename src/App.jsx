b5c8188d7f7219df05892bc1ec0a6655290c9d2b
Edward Telecan <Devedwardt@gmail.com>
2024-03-12 16:12:32 +0200

    updated case sensitive name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import Navigation from "./components/navigation/navigation";
+import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
